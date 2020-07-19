ffd4c26af9ff77baf4ef5c2c891c8ae7f850ceec
Alijah Green <atra2396@gmail.com>
2020-07-19 14:06:20 -0400

    Finished basic document controller

--- src/READUS/Controllers/DocumentController.cs
+++ src/READUS/Controllers/DocumentController.cs
@@ -3,47 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DomainObjects;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Storage;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace READUS.Controllers
 {
     [Route("api/[controller]")]
-    public class DocumentController : Controller
+    public class DocumentController : ControllerBase
     {
-        public DocumentController()
+        private IDataContext dataContext;
 
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public DocumentController(IDataContext dataContext)
         {
-            return new string[] { "value1", "value2" };
+            this.dataContext = dataContext;
         }
 
-        // GET api/<controller>/5
         [HttpGet("{id}")]
-        public Document Get(Guid id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(Guid id)
         {
-            return "value";
+            var doc = this.dataContext.Documents.Get(id);
+
+            if (doc == null)
+                return NotFound();
+
+            return Ok(doc);
         }
 
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Document document)
         {
+            if (document == null)
+                return BadRequest();
+
+            this.dataContext.Documents.Add(document);
+            return Ok(document.Id);
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put([FromBody]Document updatedDocument)
         {
+            if (updatedDocument == null)
+                return BadRequest();
+
+            this.dataContext.Documents.Update(updatedDocument);
+            return Ok();
+
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
+            var doc = this.dataContext.Documents.Get(id);
+            if (doc == null)
+                return NotFound();
+
+            this.dataContext.Documents.Delete(doc);
+            return Ok();
         }
     }
 }
