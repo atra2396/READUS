3bd0c740d0fde28fe5e19f7a3443dcda544de499
Alijah Green <atra2396@gmail.com>
2020-07-22 07:57:00 -0400

    Add organization and repository controllers

---
+++ src/READUS/Controllers/OrganizationController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Storage;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace READUS.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+        private IDataContext dataContext;
+
+        public OrganizationController(IDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(Guid id)
+        {
+            var org = this.dataContext.Organizations.GetById(id);
+            if (org == null)
+                return NotFound();
+
+            return Ok(org);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]Organization Organization)
+        {
+            if (Organization == null)
+                return BadRequest();
+
+            this.dataContext.Organizations.Add(Organization);
+            return Ok(Organization.Id);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody]Organization updatedOrganization)
+        {
+            if (updatedOrganization == null)
+                return BadRequest();
+
+            this.dataContext.Organizations.Update(updatedOrganization);
+            return Ok();
+
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var org = this.dataContext.Organizations.GetById(id);
+            if (org == null)
+                return NotFound();
+
+            this.dataContext.Organizations.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ src/READUS/Controllers/RepositoryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Storage;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace READUS.Controllers
+{
+    [Route("api/[controller]")]
+    public class RepositoryController : ControllerBase
+    {
+        private IDataContext dataContext;
+
+        public RepositoryController(IDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(Guid id)
+        {
+            var repo = this.dataContext.Repositories.GetById(id);
+            if (repo == null)
+                return NotFound();
+
+            return Ok(repo);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]Repository Repository)
+        {
+            if (Repository == null)
+                return BadRequest();
+
+            this.dataContext.Repositories.Add(Repository);
+            return Ok(Repository.Id);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody]Repository updatedRepository)
+        {
+            if (updatedRepository == null)
+                return BadRequest();
+
+            this.dataContext.Repositories.Update(updatedRepository);
+            return Ok();
+
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var repo = this.dataContext.Repositories.GetById(id);
+            if (repo == null)
+                return NotFound();
+
+            this.dataContext.Repositories.Delete(id);
+            return Ok();
+        }
+    }
+}
