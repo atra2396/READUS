884d3a626887336eddba5929ea25be6822ac1176
Alijah Green <atra2396@gmail.com>
2020-07-22 20:54:48 -0400

    super basic user entity

---
+++ src/DomainObjects/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainObjects
+{
+    public class User : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+        public string Email { get; set; }
+        public string EncryptedPassword { get; set; }
+        public IEnumerable<Guid> Organizations { get; set; }
+    }
+}
