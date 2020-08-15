06b709ad288115ff3efece838fe7eabfbff765d0
Alijah Green <atra2396@gmail.com>
2020-08-15 19:33:59 -0400

    Start parameterzing crypto settings

---
+++ src/READUS/Config/CryptoConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace READUS.Config
+{
+    public class CryptoConfig
+    {
+        public string SymmetricKey { get; set; }
+        public string Salt { get; set; }
+    }
+}
