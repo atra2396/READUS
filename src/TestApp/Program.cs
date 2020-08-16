965c99a400b0d8df632921ab5f4a63a5cd367f11
Alijah Green <atra2396@gmail.com>
2020-08-16 11:04:28 -0400

    Adding tests

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -17,6 +17,7 @@ namespace TestApp
             var docs = new MemoryRepository<Document>();
             var repos = new MemoryRepository<Repository>();
             var orgs = new MemoryRepository<Organization>();
+            var users = new MemoryRepository<User>();
 
             var newOrg = new Organization()
             {
@@ -43,7 +44,7 @@ namespace TestApp
 
             repos.Add(newRepo);
 
-            var db = new MemoryDataContext(docs, orgs, repos);
+            var db = new MemoryDataContext(docs, orgs, repos, users);
 
             var queue = new MemoryQueue<RepositoryUpdatedMessage>();
 
